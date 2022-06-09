12b90f727a7ee736ffcbd154bef1385d067cbc18
Sloane Tribble <SloaneTribble@gmail.com>
2022-06-09 16:16:59 -0700

    Add source map

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Sup");
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
