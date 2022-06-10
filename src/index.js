e266c658cc71248a3f5d0042df714a28e09edd3d
Sloane Tribble <SloaneTribble@gmail.com>
2022-06-10 15:30:24 -0700

    Add basic header button functionality

--- src/index.js
+++ src/index.js
@@ -15,5 +15,21 @@ const mainContentContainer = mainContent();
 
 contentBody.appendChild(mainContentContainer);
 
+const homeButton = document.querySelector(".home-tab");
+homeButton.addEventListener("click", ()=>{
+    console.log("Home");
+});
+
+const menuButton = document.querySelector(".menu-tab");
+menuButton.addEventListener("click", ()=>{
+    console.log("Menu");
+});
+
+const contactButton = document.querySelector(".contact-tab");
+contactButton.addEventListener("click", ()=>{
+    console.log("Contact");
+});
+
+
 
 
