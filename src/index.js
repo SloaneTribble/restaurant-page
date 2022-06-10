94ee4ce74c6ce56ab27c42f8f1fbeb951f643e45
Sloane Tribble <SloaneTribble@gmail.com>
2022-06-09 17:14:27 -0700

    Add image loading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
-import "./home.js"
+import "./home.js";
+import waluigi from './images/waluigi.jpeg';
 
 function component() {
 const element = document.createElement('div');
@@ -7,6 +8,11 @@ const element = document.createElement('div');
 // Lodash, now imported by this script
 element.classList.add('hello');
 element.textContent = "Sup";
+
+const pic = new Image();
+pic.src = waluigi;
+
+element.appendChild(pic);
 
 return element;
 }
